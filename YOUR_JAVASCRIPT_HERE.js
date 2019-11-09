8a8681d04b1db3d5d87cd7bb0dd9ca5cf5890686
ilteelte <teomitioglo@gmail.com>
2019-11-09 19:58:36 +0100

    at some level but for sure I'm not confident

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -15,20 +15,56 @@ const hero = {
 function rest(person) {
     if (hero.health === 10) {
         alert('the health property already has the value 10')      //' + ${person} + ' 
+    } else {
+        hero.health = 10
     }
     return person
 }
 
-rest(hero).health = 10
+function resetTheHealthProperty() {
+    hero.health = 10
+}
+
+// adding in the inventory array to the `type`: `dagger` and `damage`: 2
+function clickOnDagger(params) {
+    hero.inventory.push(hero.weapon)
+    hero.inventory[0].type = 'dagger'
+    hero.inventory[0].damage = 2
+}
 
 function pickUpItem(person, weapon) {
-    
+    hero.inventory.push(hero.weapon)
+
+}
+
+function clickOnBag(params) {
+    hero.inventory[0].type = 'dagger'
+    hero.inventory[0].damage = 2
+
 }
 
 function equipWeapon(person) {
-    
+    hero.inventory.push(hero.weapon)
 }
 
 function checkLeaks(params) {
     rest(hero).health = 10
 }
+
+// getting the thml elements...
+var headingElement = document.getElementById("userName");
+var healthStat = document.getElementById("health")
+var inventoryStat = document.getElementById("inventory")
+console.log(headingElement.innerHTML);
+var newHeadingText = prompt("What is your name?");
+
+// some checking...
+if (newHeadingText[0] !== newHeadingText[0].toUpperCase()) {
+    var nameCapitalized = newHeadingText.charAt(0).toUpperCase() + newHeadingText.slice(1)   
+} else {
+    var nameCapitalized = newHeadingText
+}
+headingElement.innerHTML = "User: " + nameCapitalized;
+healthStat.innerHTML = "current health: " + hero.health;
+inventoryStat.innerHTML = "current inventory: " + hero.inventory[0];
+
